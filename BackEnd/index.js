f647f1af65f0bb5d16d62fc47fa8ce0c902608ee
ahmed soumri <you@example.com>
2024-06-24 01:48:58 +0100

    implement all the routes in index

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -15,7 +15,16 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
-
+app.use('/api/products/:productId/reviews', require('./routes/reviewRoutes'));
+app.use('/api/categories', require('./routes/categoryRoutes'));
+app.use('/api/orders', require('./routes/orderRoutes'));
+app.use('/api/discounts', require('./routes/discountRoutes'));
+app.use('/api/notifications', require('./routes/notificationRoutes'));
+app.use('/api/carousel', require('./routes/carouselRoutes'));
+app.use('/api/stock-alerts', require('./routes/stockAlertRoutes'));
+app.use('/api/admin', require('./routes/adminRoutes'));
+app.use('/api/client', require('./routes/clientRoutes'));
+app.use('/api/password-reset', require('./routes/passwordResetRoutes'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
