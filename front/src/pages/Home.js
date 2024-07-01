d0dda1964ad86e1e570b5f748e177aa496c36372
ahmed soumri <you@example.com>
2024-07-01 14:47:09 +0100

    update manifest file and add logo

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,14 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 export default function Home() {
   return (
-    <div  >
-      
+    <div>
+      <Header />
 
- ssss
-
-
-
+      <Footer />
     </div>
-  )
+  );
 }
